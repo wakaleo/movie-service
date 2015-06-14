1e45b810f3b104abdcdedcaf90ab69249ac82118
John Ferguson Smart <john.smart@wakaleo.com>
2015-06-14 20:57:47 +0100

    Added admin services

--- src/main/java/com/wakaleo/myflix/movies/MovieController.java
+++ src/main/java/com/wakaleo/myflix/movies/MovieController.java
@@ -1,5 +1,7 @@
 package com.wakaleo.myflix.movies;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.wakaleo.myflix.movies.model.Movie;
 import com.wakaleo.myflix.movies.model.MovieNotFound;
 import com.wakaleo.myflix.movies.repository.MovieRepository;
@@ -22,8 +24,8 @@ public class MovieController {
     @Autowired
     private MovieRepository repository;
 
-    @RequestMapping(value = "/{id}", method=GET)
-    @ApiOperation(value = "Find a specific movie", httpMethod="GET")
+    @RequestMapping(value = "/{id}", method = GET)
+    @ApiOperation(value = "Find a specific movie", httpMethod = "GET")
     public Movie findById(@PathVariable String id) {
         Movie movie = repository.findOne(id);
         if (movie == null) {
@@ -32,30 +34,49 @@ public class MovieController {
         return movie;
     }
 
-    @RequestMapping(value="/findByDirector/{director}", method=GET)
+    @RequestMapping(value = "/findByDirector/{director}", method = GET)
     @ApiOperation(value = "Find movies from a given director",
-                  httpMethod="GET",
-                  response = Movie.class,
-                  responseContainer = "List")
+            httpMethod = "GET",
+            response = Movie.class,
+            responseContainer = "List")
     public List<Movie> findByDirector(@PathVariable String director) {
         return repository.findByDirector(capitalizeFully(director.trim()));
     }
 
-    @RequestMapping(method=GET)
+    @RequestMapping(method = GET)
     @ApiOperation("List all the movies in the catalog")
     public List<Movie> findAll() {
         return repository.findAll();
     }
 
-    @RequestMapping(method=POST)
+    @RequestMapping(method = POST)
     @ApiOperation(value = "Add a new movie to the catalog", httpMethod = "POST")
     public Movie add(@RequestBody Movie newMovie) {
         return repository.save(newMovie);
     }
 
-    @RequestMapping(value = "/{id}", method=DELETE)
+    @RequestMapping(value = "/{id}", method = DELETE)
     @ApiOperation(value = "Removing a movie from the catalog", httpMethod = "DELETE")
     public void delete(@PathVariable String id) {
         repository.delete(id);
     }
+
+    Movie GLADIATOR = new Movie("Gladiator", "Sword and sandles", "Ridley Scott", ImmutableList.of("Russel Crowe", "Joaquin Phoenix"));
+    Movie LETTERS_FROM_IWO_JIMA = new Movie("Letters from Iwo Jima", "The story of the battle of Iwo Jima...", "Clint Eastwood", ImmutableList.of("Ken Watanabe"));
+    Movie GRAN_TORINO = new Movie("Gran Torino", "Disgruntled Korean War veteran", "Clint Eastwood", ImmutableList.of("Clint Eastwood", "Bee Vang"));
+
+    @RequestMapping(value = "/admin/seed", method = GET)
+    @ApiOperation(value = "Initialize the movie catalog", httpMethod = "GET")
+    public String seedDatabase() {
+        repository.save(ImmutableList.of(GLADIATOR, LETTERS_FROM_IWO_JIMA, GRAN_TORINO));
+        return "DATABASE INITIALIZED";
+    }
+
+    @RequestMapping(value = "/admin/clean", method = GET)
+    @ApiOperation(value = "Clear the movie catalog", httpMethod = "GET")
+    public String cleanDatabase() {
+        repository.deleteAll();
+        return "DATABASE CLEANED";
+    }
+
 }
