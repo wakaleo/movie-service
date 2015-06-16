ad36fee465d47a4233dce78bd222f6afd6cf2334
John Ferguson Smart <john.smart@wakaleo.com>
2015-06-16 19:03:13 +0100

    Prepared for demo

--- src/main/java/com/wakaleo/myflix/movies/model/UnknownArtist.java
+++
@@ -1,7 +0,0 @@
-package com.wakaleo.myflix.movies.model;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="No such artist")
-public class UnknownArtist extends RuntimeException {}
--- src/test/groovy/com/wakaleo/myflix/movies/integration/WhenFindingMovies.groovy
+++ src/test/groovy/com/wakaleo/myflix/movies/integration/WhenFindingMovies.groovy
@@ -14,7 +14,8 @@ import spock.lang.Specification
 
 import static com.jayway.restassured.RestAssured.when;
 
-@ContextConfiguration(loader = SpringApplicationContextLoader.class, classes = MovieServiceApplication.class)
+@ContextConfiguration(loader = SpringApplicationContextLoader.class,
+                      classes = MovieServiceApplication.class)
 @WebAppConfiguration
 @IntegrationTest("server.port:0")
 class WhenFindingMovies extends Specification {
@@ -27,6 +28,8 @@ class WhenFindingMovies extends Specification {
 
     def GLADIATOR = new Movie(title:"Gladiator", director:"Ridley Scott",
                               description:"Sword and sandles", actors:["Russell Crowe","Joaquin Phoenix"]);
+    def THE_GOOD_THE_BAD_AND_THE_UGLY = new Movie(title:"The Good, the Bad and the Ugly", director:"Sergio Leone",
+            description:" Spaghetti Western", actors:["Clint Eastwood"]);
     def LETTERS_FROM_IWO_JIMA = new Movie(title:"Letters from Iwo Jima", director:"Clint Eastwood",
                                           description:"The story of the battle of Iwo Jima...", actors:["Ken Watanabe"]);
     def GRAN_TORINO = new Movie(title:"Gran Torino", director:"Clint Eastwood",
